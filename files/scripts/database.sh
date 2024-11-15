a9132788c279c889a78baf0bd730eb15780c7f61
COMEXR B.V. <64539804+comexr@users.noreply.github.com>
2024-11-15 16:23:06 +0100

    Enable postgres container service

--- files/scripts/database.sh
+++ files/scripts/database.sh
@@ -6,17 +6,17 @@
 set -oue pipefail
 
 # Setup database
-sudo dnf install -y postgresql-server
-sudo postgresql-setup --initdb #Initialize
-sudo systemctl enable --now postgresql #Enable and start postgresql server
-sudo -u postgres createuser $USER #Create DB user
-sudo -u postgres createdb -O $USER netxms #Create DB for user
-sudo sed -i "1s/^/host netxms $USER localhost trust\n/" /var/lib/pgsql/data/pg_hba.conf #Allow local user DB auth
+# sudo dnf install -y postgresql-server
+# sudo postgresql-setup --initdb #Initialize
+# sudo systemctl enable --now postgresql #Enable and start postgresql server
+# sudo -u postgres createuser $USER #Create DB user
+# sudo -u postgres createdb -O $USER netxms #Create DB for user
+# sudo sed -i "1s/^/host netxms $USER localhost trust\n/" /var/lib/pgsql/data/pg_hba.conf #Allow local user DB auth
 
 #Connect database
-echo -e "\nDBDriver=pgsql\nDBName=netxms\nDBLogin=$USER" | sudo tee -a /etc/netxmsd.conf
-sudo systemctl restart postgresql
+# echo -e "\nDBDriver=pgsql\nDBName=netxms\nDBLogin=$USER" | sudo tee -a /etc/netxmsd.conf
+# sudo systemctl restart postgresql
 nxdbmgr init pgsql
 
 #Shutdown database after netxms
-sudo mkdir -p /etc/systemd/system/netxms-server.service.d && echo -e "[Unit]\nAfter=network.target postgresql.service" | sudo tee /etc/systemd/system/netxms-server.service.d/override.conf
+# sudo mkdir -p /etc/systemd/system/netxms-server.service.d && echo -e "[Unit]\nAfter=network.target postgresql.service" | sudo tee /etc/systemd/system/netxms-server.service.d/override.conf
