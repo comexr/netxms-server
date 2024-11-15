1ad77736e97cbe0fb0bf850f1f3961e1d1a2f05a
COMEXR B.V. <64539804+comexr@users.noreply.github.com>
2024-11-15 15:10:46 +0100

    Add quadlet

--- files/scripts/postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-# Setup database
-podman run --name postgres -e POSTGRES_USER=username -e POSTGRES_PASSWORD=password -p 5432:5432 -v /var/lib/data -d postgres
